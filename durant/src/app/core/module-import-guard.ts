8d59607f9d06d0908248d552a641ce2745903da5
yangjie <jie.yang@witontek.com>
2020-09-08 22:33:20 +0800

    update--

--- durant/src/app/core/module-import-guard.ts
+++ durant/src/app/core/module-import-guard.ts
@@ -1,4 +1,5 @@
 // https://angular.io/guide/styleguide#style-04-12
+// 确保 核心模块等 只被加载一次
 export function throwIfAlreadyLoaded(parentModule: any, moduleName: string) {
   if (parentModule) {
     throw new Error(`${moduleName} has already been loaded. Import Core modules in the AppModule only.`);
