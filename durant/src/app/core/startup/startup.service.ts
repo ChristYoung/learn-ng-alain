7c7929e8e9a7cba4ba41a602976fa67571a09a17
yangjie <jie.yang@witontek.com>
2020-11-22 18:12:06 +0800

    🎨

--- durant/src/app/core/startup/startup.service.ts
+++ durant/src/app/core/startup/startup.service.ts
@@ -63,6 +63,7 @@ export class StartupService {
   }
 
   load(): Promise<any> {
+    console.log(location.href);
     // only works with promises
     // https://github.com/angular/angular/issues/15088
     return new Promise((resolve, reject) => {
