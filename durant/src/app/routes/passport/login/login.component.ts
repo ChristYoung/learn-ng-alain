e42a4ffc2691f8a063a704a1198f95b6629a39b3
yangjie <jie.yang@witontek.com>
2021-03-10 09:52:58 +0800

    🚧:

--- durant/src/app/routes/passport/login/login.component.ts
+++ durant/src/app/routes/passport/login/login.component.ts
@@ -125,7 +125,6 @@ export class UserLoginComponent implements OnDestroy {
         // 重新获取 StartupService 内容，我们始终认为应用信息一般都会受当前用户授权范围而影响
         this.startupSrv.load().then(() => {
           let url = this.tokenService.referrer.url || '/';
-          console.log("submit -> this.tokenService.referrer.url", this.tokenService.referrer.url);
           if (url.includes('/passport')) {
             url = '/';
           }
